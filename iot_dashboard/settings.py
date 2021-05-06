eae9c023c4b9a49e291808105c76b924c6bf6e86
Retrocamara42 <juan.manuel.neyra@outlook.com>
2021-05-05 23:36:02 -0500

    Allowed host removed

--- iot_dashboard/settings.py
+++ iot_dashboard/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['thing-hub.us-east-1.elasticbeanstalk.com']
 
 
 # Application definition
