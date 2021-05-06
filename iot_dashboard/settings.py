11c7d3119e920f61a3a35776ac13ccfffedd827f
Retrocamara42 <juan.manuel.neyra@outlook.com>
2021-05-05 23:54:24 -0500

    New allowed host added

--- iot_dashboard/settings.py
+++ iot_dashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
+ALLOWED_HOSTS=['ec2-3-134-82-241.us-east-2.compute.amazonaws.com']
 
 # Application definition
 
