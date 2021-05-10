a7c46051c67b5878509eaa40e73cade724c700d1
Retrocamara42 <juan.manuel.neyra@outlook.com>
2021-05-10 18:53:50 -0500

    Allowed host is env variable

--- iot_dashboard/settings.py
+++ iot_dashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['ec2-3-134-82-241.us-east-2.compute.amazonaws.com']
+ALLOWED_HOSTS=[os.getenv('HOST_DNS_NAME')]
 
 # Application definition
 
