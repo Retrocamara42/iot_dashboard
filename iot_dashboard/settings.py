9a0cd1621a014f1c354900abf331b4b7ab5cc4b7
Retrocamara42 <juan.manuel.neyra@outlook.com>
2021-06-05 00:11:29 -0500

    Restoring add hosts settings

--- iot_dashboard/settings.py
+++ iot_dashboard/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-#ALLOWED_HOSTS=[os.getenv('HOST_NAME')]
+ALLOWED_HOSTS=[os.getenv('HOST_NAME')]
 
 # Application definition
 
